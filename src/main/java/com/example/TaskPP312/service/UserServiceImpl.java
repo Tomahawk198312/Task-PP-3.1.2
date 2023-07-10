2c6fb4e3218bc02ec86a3ae818cca02e6cda3833
Tomahawk198312 <129873302+Tomahawk198312@users.noreply.github.com>
2023-07-10 19:32:16 +0500

    Fix

--- src/main/java/com/example/TaskPP312/service/UserServiceImpl.java
+++ src/main/java/com/example/TaskPP312/service/UserServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.TaskPP312.model.User;
 import com.example.TaskPP312.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
